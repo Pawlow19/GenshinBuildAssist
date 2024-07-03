75b355322d039d0e77b209afc7ed7ea8b1d41939
Pawlow19 <pawlow19i@gmail.com>
2024-07-04 01:30:58 +0200

    a
    a

---
+++ scripts/preload.js
@@ -0,0 +1,22 @@
+const backgrounds = [
+    'assets/backgrounds/BG_Fontaine.webp',
+    'assets/backgrounds/BG_Inazuma.webp',
+    'assets/backgrounds/BG_Liyue.webp',
+    'assets/backgrounds/BG_Mond.webp',
+    'assets/backgrounds/BG_Starry.webp',
+    'assets/backgrounds/BG_Sumeru_Desert.webp',
+    'assets/backgrounds/BG_Sumeru_Rainforest.webp'
+];
+
+// Funkcja do wczytywania obrazów do cache
+function preloadImages() {
+    backgrounds.forEach(imageUrl => {
+        const img = new Image();
+        img.src = imageUrl;
+        img.onload = () => {
+            console.log(`Preloaded: ${imageUrl}`);
+        };
+    });
+}
+
+preloadImages();
