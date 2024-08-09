eaaebe984ea39bafa9f85ad3aefdba3dab168f45
Pawlow19 <pawlow19i@gmail.com>
2024-08-09 02:13:35 +0200

    a
    a

--- scripts/character_detail.js
+++ scripts/character_detail.js
@@ -51,7 +51,7 @@ function setCharacterBackground(character) {
     document.body.style.backgroundImage = `url('assets/backgrounds/${backgroundData.file}')`;
 
     const backgroundAuthor = document.getElementById("background-author");
-    backgroundAuthor.innerText = `OG File: ${backgroundData.author}`;
+    backgroundAuthor.innerText = backgroundData.author;
 }
 
 function loadCharacterFromLocalStorage(character) {
